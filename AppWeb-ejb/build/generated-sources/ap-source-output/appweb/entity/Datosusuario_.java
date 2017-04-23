82cfe02fd849f967024621b77f51bddf4857de28
Garlo13git config --global user.email adri_presim301@hotmail.comgit config --global user.name Garlo13 <adri_presim301@hotmail.com>
2017-04-23 12:20:33 +0200

    Añadido .gitignore

--- AppWeb-ejb/build/generated-sources/ap-source-output/appweb/entity/Datosusuario_.java
+++ AppWeb-ejb/build/generated-sources/ap-source-output/appweb/entity/Datosusuario_.java
@@ -12,7 +12,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-21T17:07:12")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-23T11:31:40")
 @StaticMetamodel(Datosusuario.class)
 public class Datosusuario_ { 
 
--- AppWeb-ejb/build/generated-sources/ap-source-output/appweb/entity/Mensaje_.java
+++ AppWeb-ejb/build/generated-sources/ap-source-output/appweb/entity/Mensaje_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-21T17:07:12")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-23T11:31:40")
 @StaticMetamodel(Mensaje.class)
 public class Mensaje_ { 
 
